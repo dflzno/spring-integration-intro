4f1bece37791e92dae4f0cc46a1f96c5c598097f
David <davidlozano05@gmail.com>
2016-08-29 15:22:35 -0500

    Support for lightweight payloads

--- SpringIntegrationWithJMS/src/main/java/com/geekcap/springintegrationexample/service/RetrievePayloadService.java
+++ SpringIntegrationWithJMS/src/main/java/com/geekcap/springintegrationexample/service/RetrievePayloadService.java
@@ -1,6 +1,12 @@
 package com.geekcap.springintegrationexample.service;
 
+/***
+ * This interface might be used in the case of a lightweight message posted by a
+ * producer with just a resource id. The implementations will need to go back to
+ * the SOR (System Of Record) and retrieve the payload (represented by a String
+ * for this case)
+ */
 public interface RetrievePayloadService {
-	
+
 	public String getPayload(String id);
 }
