a8d5e05411c54a944c1e15a8bc8867885481bf0e
David <davidlozano05@gmail.com>
2016-09-13 16:18:36 -0500

    configuring message channels

--- HelloSpringIntegrationAnnotationDriven/src/main/java/app/configuration/InfrastructureConfiguration.java
+++ HelloSpringIntegrationAnnotationDriven/src/main/java/app/configuration/InfrastructureConfiguration.java
@@ -1,12 +1,27 @@
 package app.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.messaging.MessageChannel;
 
 @Configuration
 @EnableIntegration
 @ComponentScan(basePackages = "app")
+@IntegrationComponentScan(basePackages = "app")
 public class InfrastructureConfiguration {
+	
+	@Bean
+	public MessageChannel helloWorldChannel() {
+		return new DirectChannel();
+	}
 
+	@Bean
+	public MessageChannel helloWorldWithReplyChannel() {
+		return new DirectChannel();
+	}
 }
+ 
